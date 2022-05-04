19e0709fa17767f7f3cd8f79a495949ef85b7ed0
suenilton <sueniltonjunior@gmail.com>
2022-05-04 11:24:32 -0300

    alteração nos diretórios do projeto

--- alurareceita/settings.py
+++ alurareceita/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
-from pathlib import Path, os
+from pathlib import Path, os, sys
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -138,3 +138,7 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger',
     messages.SUCCESS: 'success',
 }
+
+# Alterando a configuração da pasta raiz do apps do projeto.
+PROJECT_ROOT = os.path.dirname(__file__)
+sys.path.insert(0, os.path.join(PROJECT_ROOT,'..\\apps'))
